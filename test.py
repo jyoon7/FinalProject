8506e8fe13e70681cdff0f551c2a6f8c544e60ba
jyoon7 <55115653+jyoon7@users.noreply.github.com>
2019-12-12 21:59:31 -0500

    fix

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+from mutagen.id3 import ID3
 import pygame
 from tkinter import *
 from tkinter.filedialog import askdirectory
@@ -8,6 +9,9 @@ root.title("Music Player")
 root.minsize(300,300)
 
 listofsongs = []
+realnames =[]
+index = 0
+
 
 def directorychooser():
     directory = askdirectory()
@@ -15,15 +19,42 @@ def directorychooser():
 
     for files in os.listdir(directory):
         if files.endswith(".mp3"):
+            realdir = os.path.realpath(files)
+            audio = ID3(realdir)
+            realnames.append(audio['TIT2'].text[0])
             listofsongs.append(files)
     
     print(listofsongs)
     pygame.init()
     pygame.mixer.init()
-    pygame.mixer.music.load(listofsongs[0])
+    pygame.mixer.music.load(listofsongs[index])
     pygame.mixer.music.play()
 
 directorychooser()
+realnames.reverse()
+
+def nextsong(event):
+    global index
+    index += 1
+    pygame.mixer.music.load(listofsongs[index])
+    pygame.mixer.music.play    
+    print(index)
+
+def prevsong(event):
+    global index
+    index -= 1
+    pygame.mixer.music.load(listofsongs[index])
+    pygame.mixer.music.play
+    print(index)
+
+def playsong(event):
+    pygame.mixer.music.play
+    print(index)
+
+def stopsong(event):
+    pygame.mixer.music.stop
+    print(index)
+    
 
 
 label = Label(root, text="Music player")
@@ -33,7 +64,7 @@ label.pack()
 listbox = Listbox(root)
 listbox.pack()
 
-for items in listofsongs:
+for items in realnames:
     listbox.insert(0, items)
 
 next = Button(root, text = 'Next')
@@ -42,8 +73,18 @@ next.pack()
 prev = Button(root, text = 'Previous')
 prev.pack()
 
+play = Button(root, text = 'Play')
+play.pack()
+
 stop = Button(root, text = 'Stop')
 stop.pack()
+
+next.bind("<Button-1>",nextsong)
+prev.bind("<Button-1>",prevsong)
+play.bind("<Button-1>",playsong)
+stop.bind("<Button-1>",stopsong)
+
+
 
 
 # btn = Button(root, text="Select folder", command = directorychooser)
