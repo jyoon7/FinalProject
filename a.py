9c6afeb36f8f8d2d5eea4178c015f1af0bb2735e
jyoon7 <jyoon7@berklee.edu>
2019-12-19 18:45:05 -0500

    a

---
+++ a.py
@@ -0,0 +1,6 @@
+a = [[] for i in range(10)]
+
+a.append("1")
+a[0].append("2")
+print(a[0])
+print(a[0][0])
--- test.py
+++ test.py
@@ -6,56 +6,37 @@ from tkinter import *
 from tkinter.filedialog import askdirectory
 
 root = Tk()
-root.title("Music Player")
+root.title("test")
 root.minsize(300,300)
 mixer.init()
 
-listofsongs = []
-realnames =[]
+song = []
+title = []
+artist = []
+
 index = 0
 
 
-def directorychooser():
-    directory = askdirectory()
-    os.chdir(directory)
 
-    for files in os.listdir(directory):
-        if files.endswith(".mp3"):
-            realdir = os.path.realpath(files)
-            audio = ID3(realdir)
-            realnames.append(audio['TIT2'].text[0])
-            listofsongs.append(files)
-    
-    print(listofsongs)
-    pygame.init()
-    pygame.mixer.init()
-    pygame.mixer.music.load(listofsongs[0])
-    pygame.mixer.music.play()
+directory = askdirectory()
+os.chdir(directory)
 
-directorychooser()
+for files in os.listdir(directory):
+    x = 0
+    if files.endswith(".mp3"):
+        realdir = os.path.realpath(files)
+        audio = ID3(realdir)
+        
+        song.append(files)
+        title.append(audio['TIT2'].text[0])
+        artist.append(audio['TPE1'].text[0])
+        # listofsongs.append(audio['TPE3'].text[0])
+        x += 1
 
-def nextsong(event):
-    global index
-    index += 1
-    pygame.mixer.music.load(listofsongs[index])
-    pygame.mixer.music.play()
-    print(index)
+print(song[0])
+print(title[0])
+print(artist[0])
 
-def prevsong(event):
-    global index
-    index -= 1
-    pygame.mixer.music.load(listofsongs[index])
-    pygame.mixer.music.play()
-    print(index)
-
-def playsong(event):
-    pygame.mixer.music.play()
-    print(index)
-
-def stopsong(event):
-    pygame.mixer.music.stop()
-    print(index)
-    
 
 
 label = Label(root, text="Music player")
@@ -65,34 +46,13 @@ label.pack()
 listbox = Listbox(root)
 listbox.pack()
 
-realnames.reverse()
-for items in realnames:
-    listbox.insert(0, items)
-realnames.reverse()
-
-nextbutton = Button(root, text = 'Next')
-nextbutton.pack()
-
-prev = Button(root, text = 'Previous')
-prev.pack()
-
-play = Button(root, text = 'Play')
-play.pack()
-
-stop = Button(root, text = 'Stop')
-stop.pack()
-
-nextbutton.bind("<Button-1>",nextsong)
-prev.bind("<Button-1>",prevsong)
-play.bind("<Button-1>",playsong)
-stop.bind("<Button-1>",stopsong)
+# realnames.reverse()
+# for items in realnames:
+#     listbox.insert(0, items)
+# realnames.reverse()
 
 
 
-
-# btn = Button(root, text="Select folder", command = directorychooser)
-
-# # btn.grid(column=1, row=0)
 
 
 root.mainloop()
